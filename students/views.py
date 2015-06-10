f0723719c97ce8298d0d990b0b279fbaa3b29d2b
Isizukuri <ragnarok49@gmail.com>
2015-06-10 14:10:25 +0300

    Added groups.html template and linked it

--- students/views.py
+++ students/views.py
@@ -3,6 +3,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#Students views
+
 def students_list(request):
    students = (
    {'id': 1,
@@ -30,9 +32,11 @@ def students_edit(request, sid):
    
 def students_delete(request, sid):
    return HttpResponse('<h1>Delete Student %s</h1>' % sid)
+
+# Group viwews
    
 def groups_list(request):
-   return HttpResponse('<h1>Groups Listing</h1>')
+   return render(request, 'students/groups.html', {})
    
 def groups_add(request):
    return HttpResponse('<h1>Group Add Form</h1>')
