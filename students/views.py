4b3ff22a3aba1f01669fdad7c61abc1be7a05e67
Isizukuri <ragnarok49@gmail.com>
2015-08-18 09:04:46 +0300

    Added separate views for templates.

--- students/views.py
+++ students/views.py
@@ -3,46 +3,4 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-#Students views
 
-def students_list(request):
-   students = (
-   {'id': 1,
-   'first_name': u'Віталій',
-   'last_name': u'Подоба',
-   'ticket': 235,
-   'image': 'img/ueshiba1.jpeg'},
-   {'id': 2,
-   'first_name': u'Корост',
-   'last_name': u'Андрій',
-   'ticket': 2123,
-   'image': 'img/takeda.jpg'},
-   {'id' : 3,
-   'first_name': u'Олександр',
-   'last_name': u'Павлюк',
-   'ticket': 235,
-   'image': 'img/morihei-ueshiba2.jpg'})
-   return render(request, 'students/student_list.html', {'students': students})
-   
-def students_add(request):
-   return HttpResponse('<h1>Student Add Form</h1>')
-   
-def students_edit(request, sid):
-   return HttpResponse('<h1>Edit Student %s</h1>' % sid)
-   
-def students_delete(request, sid):
-   return HttpResponse('<h1>Delete Student %s</h1>' % sid)
-
-# Group viwews
-   
-def groups_list(request):
-   return render(request, 'students/groups.html', {})
-   
-def groups_add(request):
-   return HttpResponse('<h1>Group Add Form</h1>')
-   
-def groups_edit(request, gid):
-   return HttpResponse('<h1>Edit Group %s</h1>' % gid)
-   
-def groups_delete(request, gid):
-   return HttpResponse('<h1>Delete Group %s</h1>' % gid)
