3a19c999c9831501a8c8470ce7d1c3443e1e36bd
Isizukuri <ragnarok49@gmail.com>
2015-08-18 21:27:28 +0300

    Added default sorting by last name at /.

--- students/custom_views/student_list.py
+++ students/custom_views/student_list.py
@@ -10,6 +10,7 @@ def students_list(request):
    
    # try to order students list
    order_by = request.GET.get('order_by', '')
+   students = students.order_by('last_name')
    if order_by in ('last_name', 'first_name', 'ticket'):
       students = students.order_by(order_by)
       if request.GET.get('reverse', '') == '1':
