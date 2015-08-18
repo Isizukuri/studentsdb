f2991bdf3e6f60e0af9982648a2ad3dae3d5a70c
Isizukuri <ragnarok49@gmail.com>
2015-08-18 12:02:59 +0300

    Little changes.

---
+++ students/custom_views/student_list.py
@@ -0,0 +1,32 @@
+#-*- coding: utf-8 -*-
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def students_list(request):
+   students = (
+   {'id': 1,
+   'first_name': u'Віталій',
+   'last_name': u'Подоба',
+   'ticket': 235,
+   'image': 'img/ueshiba1.jpeg'},
+   {'id': 2,
+   'first_name': u'Корост',
+   'last_name': u'Андрій',
+   'ticket': 2123,
+   'image': 'img/takeda.jpg'},
+   {'id' : 3,
+   'first_name': u'Олександр',
+   'last_name': u'Павлюк',
+   'ticket': 235,
+   'image': 'img/morihei-ueshiba2.jpg'})
+   return render(request, 'students/student_list.html', {'students': students})
+   
+def students_add(request):
+   return HttpResponse('<h1>Student Add Form</h1>')
+   
+def students_edit(request, sid):
+   return HttpResponse('<h1>Edit Student %s</h1>' % sid)
+   
+def students_delete(request, sid):
+   return HttpResponse('<h1>Delete Student %s</h1>' % sid)
